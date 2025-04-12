fbc8298c6173b367856520070fff9660e4eb938f
Saad Aamir <saad6101@students.dpssgr.com>
2025-04-12 18:08:12 +0530

    main

---
+++ scripts/typed.js
@@ -0,0 +1,15 @@
+var typed = new Typed("#typed", {
+    strings: [
+        "Web Development",
+        "App Development",
+        "Programming",
+        "Robotics",
+        "Graphic Design",
+        "AI",
+        "Game Development",
+        "Digital Marketing"
+    ],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+});
